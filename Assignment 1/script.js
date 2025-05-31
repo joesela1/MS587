725d66ab9c578fcd0a0da779193ed55efd475d16
Joey Selamat <joeyselamat@gmail.com>
2025-05-30 18:53:44 -0700

    Delete Assignment 1 directory

--- Assignment 1/script.js
+++
@@ -1,9 +0,0 @@
-function show(){
-    const inform = document.getElementById('info');
-
-    if(inform.style.display === 'none'){
-        inform.style.display = 'block';
-    } else {
-        inform.style.display = 'none';
-    }
-}
